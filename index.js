d97be6983035a97d12b31ac9c542b1e0eade3d77
elaf swara <elafswara4@gmail.com>
2022-08-08 16:01:20 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    
+    const m=cats.push(name);
+    return m;
+}
+
+ 
+function destructivelyPrependCat(name){
+    
+   const m= cats.unshift(name);
+    return m;
+}
+
+
+function destructivelyRemoveLastCat(name){
+    
+    const m= cats.pop(name);
+     return m;
+ }
+
+ function destructivelyRemoveFirstCat(name){
+    
+    const m= cats.shift(name);
+     return m;
+ }
+
+ function appendCat(name){
+    
+    const m= [...cats,name]
+
+     return m;
+ }
+  function prependCat(name){
+    const m= [name,...cats]
+
+    return m;
+  }
+  function removeLastCat(){
+    const m= cats.slice(0,2);
+     return m;
+  }
+  function removeFirstCat(){
+    const m= cats.slice(1);
+     return m;
+  }
